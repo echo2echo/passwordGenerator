24ba5d67d7dcd4883ce16f4da5ed2b1fdc54b71d
Joel P. Rader <echophotographic@gmail.com>
2026-02-09 23:03:33 -0500

    Refactor password generation logic in passGen.pl
    Refactored password generation to use Crypt::RandPasswd for both word and character generation. Added variables for minimum and maximum length based on user input.

--- passGen.pl
+++ passGen.pl
@@ -4,23 +4,24 @@ use strict;
 use warnings;
 use diagnostics;
 
-# Uncomment line below if using as local $ARGV script
 use CGI;
 
 use Crypt::RandPasswd;
-
-# Uncomment $ARGV line below and comment the two $cgi lines below that to run \
-# as a local script instead of cgi script
 
 #my $in = $ARGV[0];
 my $cgi = CGI->new;
 my $in = $cgi->param("length");
 
-# Remove < and & from generated password
+my $word;
+my $minlen = $in;
+my $maxlen = $in;
+my $hyphenated;
+
+( $word, $hyphenated ) = Crypt::RandPasswd->word( $minlen, $maxlen );
+$word = Crypt::RandPasswd->chars( $minlen, $maxlen );
 $word =~ s/<//g;
 $word =~ s/&//g;
 
-
-# Also remove following line if runnug as local script
 print "Content-type: text/html\n\n";
+
 print "$word\n";
